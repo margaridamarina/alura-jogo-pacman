396c8ea2ec5ea4d8dacfbfd68bef75a71daf4a13
Margarida <margaridammcm@gmail.com>
2022-10-30 08:53:35 -0300

    ball moviment

--- pacman.py
+++ pacman.py
@@ -1,14 +1,34 @@
 import pygame
-AMARELO = (255, 255, 0)
+amarelo = (255, 255, 0)
+preto = (0, 0, 0)
+velocidade = 1
+raio = 20
+
 pygame.init()
 
 tela = pygame.display.set_mode((640,480), 0)
+x = 10
+y = 10
+vel_x = velocidade
+vel_y = velocidade
 
 while True:
     #Calcula as regras
+    x = x + vel_x
+    y = y + vel_y
+
+    if x + raio > 640:
+        vel_x = -velocidade
+    if x - raio < 0:
+        vel_x = velocidade
+    if y + raio > 480:
+        vel_y = -velocidade
+    if y - raio < 0:
+        vel_y = velocidade
 
     #Pinta
-    pygame.draw.circle(tela, AMARELO, (320, 240), 50, 0)
+    tela. fill(preto)
+    pygame.draw.circle(tela, amarelo, (int(x), int(y)), raio, 0)
     pygame.display.update()
 
     #Eventos
