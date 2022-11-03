a33ee0806fe0849eb66b0ad380429eb2e88ebdfa
Margarida <margaridammcm@gmail.com>
2022-11-03 10:21:48 -0300

    class Cenario and change variables names

--- pacman.py
+++ pacman.py
@@ -5,14 +5,60 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600), 0)
 
-vermelho = (255, 0, 0)
-preto = (0,0,0)
+red = (255,0,0)
+black = (0,0,0)
+blue = (0,0,255)
 velocidade = 1
+
+class Cenario:
+    def __init__(self, tamanho):
+        self.tamanho = tamanho
+        self.matriz = [
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 0, 0, 0, 0, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 2, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+        ]
+
+    def paint_line(self, tela, line_number, line):
+        for column_number, column in enumerate(line):
+            x = column_number * self.tamanho
+            y = line_number * self.tamanho
+            pygame.draw.rect(tela, blue, (x, y, self.tamanho, self.tamanho), 0)
+    
+    def paint(self, tela):
+        for line_number, line in enumerate(self.matriz):
+            self.paint_line(tela, line_number, line)
 
 class Pacman:
     def __init__(self):
-        self.coluna = 1
-        self.linha = 1
+        self.column = 1
+        self.line = 1
         self.centro_x = 400
         self.centro_y = 300
         self.tamanho = 800 // 30
@@ -21,27 +67,27 @@ class Pacman:
         self.raio = self.tamanho // 2
 
     def calculate_rules(self):
-        self.coluna = self.coluna + self.vel_x
-        self.linha = self.linha + self.vel_y
-        self.centro_x = int(self.coluna * (self.tamanho + self.raio))
-        self.centro_y = int(self.linha * (self.tamanho + self.raio))
+        self.column = self.column + self.vel_x
+        self.line = self.line + self.vel_y
+        self.centro_x = int(self.column * (self.tamanho + self.raio))
+        self.centro_y = int(self.line * (self.tamanho + self.raio))
 
     def paint(self, tela):
         #Draw pacman's body
-        pygame.draw.circle(tela, vermelho, (self.centro_x, self.centro_y), self.raio)
+        pygame.draw.circle(tela, red, (self.centro_x, self.centro_y), self.raio)
 
         #Draw pacman's mouth
         canto_boca = (self.centro_x, self.centro_y)
         labio_superior = (self.centro_x + self.raio, self.centro_y - self.raio)
         labio_inferior = (self.centro_x + self.raio, self.centro_y)
         pontos = [canto_boca, labio_superior, labio_inferior]
-        pygame.draw.polygon(tela, preto, pontos, 0)
+        pygame.draw.polygon(tela, black, pontos, 0)
 
         #Draw pacman's eye
         olho_x = int(self.centro_x + self.raio / 4)
         olho_y = int(self.centro_y - self.raio * 0.6)
         olho_raio = int(self.raio/10)
-        pygame.draw.circle(tela, preto, (olho_x, olho_y), olho_raio, 0)
+        pygame.draw.circle(tela, black, (olho_x, olho_y), olho_raio, 0)
 
     def process_events(self, events):
         #Capture events
@@ -70,18 +116,20 @@ class Pacman:
         for e in events:
             if e.type == pygame.MOUSEMOTION:
                 mouse_x, mouse_y = e.pos
-                self.coluna = (mouse_x - self.centro_x) / delay
-                self.linha = (mouse_y - self.centro_y) / delay
+                self.column = (mouse_x - self.centro_x) / delay
+                self.line = (mouse_y - self.centro_y) / delay
 
 if __name__ == "__main__":
     pacman = Pacman()
+    cenario = Cenario(600 // 30)
 
     while True:
         #Calculate rules
         pacman.calculate_rules()
 
         #Draw screen
-        screen.fill(preto)
+        screen.fill(black)
+        cenario.paint(screen)
         pacman.paint(screen)
         pygame.display.update()
         pygame.time.delay(100)
